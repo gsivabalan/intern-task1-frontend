60d49aaf7752a6a88ae0741bb93d2cb45c5eaff9
gsivabalan <siva90.balan@gmail.com>
2023-11-07 11:24:38 +0530

    third commit

---
+++ build/precache-manifest.96f1e53f1fd3545f5030e3fb7ea15c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567ff33d6374a7ad55b4bf6c89954fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d94e739bcd6d54279d0",
+    "url": "/static/css/main.af4c7b32.chunk.css"
+  },
+  {
+    "revision": "644e85116a296f7c6793",
+    "url": "/static/js/2.ca5d6863.chunk.js"
+  },
+  {
+    "revision": "c01479b4b07d6991f42815ae667ca6f3",
+    "url": "/static/js/2.ca5d6863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d94e739bcd6d54279d0",
+    "url": "/static/js/main.8886a810.chunk.js"
+  },
+  {
+    "revision": "940b9cb0c27bb71a470a",
+    "url": "/static/js/runtime-main.fc5eeba3.js"
+  }
+]);
